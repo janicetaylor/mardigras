8c9a681f81e6a0b2de7a14b8f922ffcb66e7aab5
Janice Taylor <notjanicetaylor@gmail.com>
2024-07-21 17:16:37 -0400

    initial commit

---
+++ MardiGras/MardiGras/MardiGrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MardiGrasApp.swift
+//  MardiGras
+//
+//  Created by Janice on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MardiGrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
